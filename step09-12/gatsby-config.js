2efec7e6a428ad71cd6fb75059b6d0c80f0c09ab
Hassan Ali Khan <h.a.khan1992fl@gmail.com>
2021-01-25 23:00:28 +0500

    Step09 (Fetch Data Build Time Contentful)
    1:- On Contentful Web
    	a:- Signup on contentfull web
    	b:- Make an organization
    	c:- Create a space in it "Content is King!"
    	d:- Create content type from content model named "Blog post"
    	e:- Add fields "text for Title", "Rich text for content" and "data and time for date" and save
    	f:- Create content as required
    	g:- From Setting create Api Key and save keys to a file
    2:- Install
    	a:- ContentFull in the app using "npm install --save gatsby-source-contentful"
    3:- Update
    	a:- gatsby-config.js to Config the gatsby-source-contentful and the copy the space ID and access token.
    4:- open graphql and create a query
    5:- Modify
    	a:- src\pages\index.tsx to add graphQl query in the app and display data in the app

--- step09-12/gatsby-config.js
+++ step09-12/gatsby-config.js
@@ -1,3 +1,12 @@
 module.exports = {
-  plugins: ["gatsby-plugin-typescript"],
+  plugins: [
+    "gatsby-plugin-typescript",
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `2w5a71lfuqa4`,
+        accessToken: `uvgiXEdKuGKQ3HmGZWGSaOnb5B3mCzEweEmIKl3f4Pc`,
+      },
+    },
+  ],
 };
--- step09-12/src/pages/index.tsx
+++ step09-12/src/pages/index.tsx
@@ -1,9 +1,39 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function home() {
+export default function home({data}) {
+  console.log(data)
   return (
-  <div> 
-      Hello world
-  </div>
+    <div>
+      <div>{data.allContentfulBlogPost.nodes[2].title}</div>
+      <div>{data.allContentfulBlogPost.nodes[2].body.raw}</div>
+      <div>{data.allContentfulBlogPost.nodes[2].publishDate}</div>
+      <br/>
+      <hr/>
+      <div>{data.allContentfulBlogPost.nodes[1].title}</div>
+      <div>{data.allContentfulBlogPost.nodes[1].body.raw}</div>
+      <div>{data.allContentfulBlogPost.nodes[1].publishDate}</div>
+      <br/>
+      <hr/>
+      <div>{data.allContentfulBlogPost.nodes[0].title}</div>
+      <div>{data.allContentfulBlogPost.nodes[0].body.raw}</div>
+      <div>{data.allContentfulBlogPost.nodes[0].publishDate}</div>
+      <br/>
+      <hr/>
+    </div>
   )
-} 
+}
+
+export const query = graphql`
+  query {
+    allContentfulBlogPost {
+      nodes {
+        title
+        body {
+          raw
+        }
+        publishDate
+      }
+    }
+  }
+`
